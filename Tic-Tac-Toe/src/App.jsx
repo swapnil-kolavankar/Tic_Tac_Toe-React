32974acfaa647c803d3322f63cecac032053e7a3
swapnil-kolavankar <swapnil.kolavankar15@gmail.com>
2024-07-03 17:11:10 +0530

    create tic-tac-toe game

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TicTacToe from './components/TicTacToe/TicTacToe'
+
+function App() {
+  
+
+  return (
+    <>
+      <TicTacToe></TicTacToe>
+    </>
+  )
+}
+
+export default App
